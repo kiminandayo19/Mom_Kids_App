54d7f7085a1b2682d49887fc860567d6f2ad681d
anindyo <anindyo0216@gmail.com>
2022-10-24 13:22:55 +0700

    feat: Add Bottom Navigation Bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,115 @@
+import 'package:mom_and_kids_app/screens/article/article_screen.dart';
+import 'package:mom_and_kids_app/screens/community/forum_screen.dart';
+import 'package:mom_and_kids_app/screens/consultation/consultation_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'screens/home/home_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        title: "mom and kids",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color(0xFFFCFCFC),
+        ),
+        home: Screens()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Screens extends StatefulWidget {
+  const Screens({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Screens> createState() => _ScreensState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ScreensState extends State<Screens> {
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  static final List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    Article(),
+    Consultation(),
+    Forum()
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      extendBodyBehindAppBar: true,
+
+      body: IndexedStack(
+          index: _selectedIndex,
+          children: _widgetOptions
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/nav_home_icon.svg"),
+            label: 'Home',
+            activeIcon: SvgPicture.asset(
+              "assets/icons/nav_home_icon.svg",
+              color: const Color(0xFF8EC3B3),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/nav_article_icon.svg"),
+            label: 'Article',
+            activeIcon: SvgPicture.asset(
+              "assets/icons/nav_article_icon.svg",
+              color: const Color(0xFF8EC3B3),
             ),
-          ],
-        ),
+          ),
+
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/nav_consultation_icon.svg"),
+            label: 'Consultation',
+            activeIcon: SvgPicture.asset(
+              "assets/icons/nav_consultation_icon.svg",
+              color: const Color(0xFF8EC3B3),
+            ),
+          ),
+
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset("assets/icons/nav_community_icon.svg"),
+            label: 'Community',
+            activeIcon: SvgPicture.asset(
+              "assets/icons/nav_community_icon.svg",
+              color: const Color(0xFF8EC3B3),
+            ),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF8EC3B3),
+        onTap: _onItemTapped,
+        selectedLabelStyle: const TextStyle(fontFamily: "Avenir-Regular", fontSize: 10.0),
+        unselectedLabelStyle: const TextStyle(fontFamily: "Avenir-Regular", fontSize: 10.0),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
